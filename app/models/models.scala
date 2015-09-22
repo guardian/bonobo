f595c718800230788cd85ce8a728a18a2d843d38
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2015-09-22 15:48:33 +0100

    Save and find in DinamoDB

---
+++ app/models/models.scala
@@ -0,0 +1,14 @@
+package models
+
+case class BonoboKeys
+(Id: String,
+ key: String,
+ name: String,
+ email: String,
+ company: String,
+ url: String,
+ requests_per_day: Int,
+ requests_per_minute: Int,
+ tier: String,
+ status: String)
+
---
+++ app/store/Dynamo.scala
@@ -0,0 +1,51 @@
+package store
+
+import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec
+import com.amazonaws.services.dynamodbv2.document.{DynamoDB, Item}
+import models._
+
+import scala.collection.JavaConverters._
+
+class Dynamo(db: DynamoDB, tableName: String) {
+
+  private val bonoboTable = db.getTable(tableName)
+
+  def save(bonoboKeys: BonoboKeys): Unit = {
+    val item = toItem(bonoboKeys)
+    bonoboTable.putItem(item)
+  }
+
+  def getAllKeys(): List[BonoboKeys] = {
+    val it = bonoboTable.scan(new ScanSpec()).iterator().asScala
+    it.map(fromItem(_)).toList
+  }
+
+  def toItem(bonoboKeys: BonoboKeys): Item = {
+    new Item()
+      .withPrimaryKey("Id", bonoboKeys.Id)
+      .withString("key", bonoboKeys.key)
+      .withString("name", bonoboKeys.name)
+      .withString("company", bonoboKeys.company)
+      .withString("email", bonoboKeys.email)
+      .withInt("requests_per_day", bonoboKeys.requests_per_day)
+      .withInt("requests_per_minute", bonoboKeys.requests_per_minute)
+      .withString("status", bonoboKeys.status)
+      .withString("tier", bonoboKeys.tier)
+      .withString("url", bonoboKeys.url)
+  }
+
+  def fromItem(item: Item): BonoboKeys = {
+    BonoboKeys(
+      Id = item.getString("Id"),
+      key = item.getString("key"),
+      name = item.getString("name"),
+      company = item.getString("company"),
+      email = item.getString("email"),
+      requests_per_day = item.getInt("requests_per_day"),
+      requests_per_minute = item.getInt("requests_per_minute"),
+      status = item.getString("status"),
+      tier = item.getString("tier"),
+      url = item.getString("url")
+    )
+  }
+}
