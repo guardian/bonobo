48c45dfefd6dac8993c57dd6ad0a869d0813d50c
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2015-11-02 11:30:30 +0000

    Fix test

--- test/controllers/ApplicationSpec.scala
+++ test/controllers/ApplicationSpec.scala
@@ -30,13 +30,12 @@ class ApplicationSpec extends FlatSpec with Matchers with MockitoSugar {
       def deleteKongKey(createdAt: String): Unit = ???
       def retrieveKey(id: String): Option[KongKey] = ???
 
-      def getKeys(direction: String, range: Option[Long]): ResultsPage[BonoboInfo] = {
+      def getKeys(direction: String, range: Option[String]): ResultsPage[BonoboInfo] = {
         ResultsPage(List(BonoboInfo(KongKey("bonoboId", "kongId", "my-new-key", 10, 1, Tier.withName("Developer").get, "Active", new DateTime(), "rangekey"), BonoboUser("id", "name", "email", "product name", "product url", "company name", Some("company url")))), false)
       }
 
       def retrieveUser(id: String): BonoboUser = ???
       def updateBonoboUser(bonoboUser: BonoboUser): Unit = ???
-
       def getAllKeysWithId(id: String): List[KongKey] = ???
       def getUserWithId(id: String): BonoboUser = ???
       def getKeyForUser(userId: String): String = ???
