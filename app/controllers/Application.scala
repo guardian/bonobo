2ebd02bbad7f6e7146e91e9ac7d6cfa412d02564
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2015-11-02 11:13:43 +0000

    Move forms and improve queries

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -158,24 +158,6 @@ class Application(dynamo: DB, kong: Kong, val messagesApi: MessagesApi, val auth
   def healthcheck = Action { Ok("OK") }
 }
 
-object Forms {
-
-  case class CreateUserFormData(name: String, email: String, productName: String, productUrl: String, companyName: String, companyUrl: Option[String], tier: Tier, key: Option[String] = None)
-
-  case class EditUserFormData(name: String, email: String, productName: String, productUrl: String, companyName: String, companyUrl: Option[String])
-
-  case class CreateKeyFormData(key: Option[String], tier: Tier)
-
-  case class EditKeyFormData(key: String, requestsPerDay: Int, requestsPerMinute: Int, tier: Tier, defaultRequests: Boolean, status: String) {
-    def validateRequests: Boolean = requestsPerDay >= requestsPerMinute
-  }
-
-  case class SearchFormData(query: String)
-
-  case class OpenCreateKeyFormData(name: String, email: String, productName: String, productUrl: String, companyName: String, companyUrl: Option[String], acceptTerms: Boolean)
-
-}
-
 object Application {
   import Forms._
 
