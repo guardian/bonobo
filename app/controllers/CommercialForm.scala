59b3ead0930638da89f44397add8b2511a100077
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2015-11-19 13:58:13 +0000

    Add send email for developer form and small fixes

--- app/controllers/CommercialForm.scala
+++ app/controllers/CommercialForm.scala
@@ -35,7 +35,7 @@ class CommercialForm(dynamo: DB, kong: Kong, awsEmail: MailClient, val messagesA
           awsEmail.sendEmailCommercialRequest(user) map {
             result => Redirect(routes.CommercialForm.requestMessage())
           } recover {
-            case _ => Redirect(routes.CommercialForm.requestMessage()).flashing("error" -> "We were unable to send the email. Please contact [email] for further instructions")
+            case _ => Redirect(routes.CommercialForm.requestMessage()).flashing("error" -> "We were unable to send the email. Please contact [email] for further instructions") //TODO: Add email address
           }
         }
       }
