c590242460725b3277abd894e6815ebe72c13c83
Luke Taylor <luke.taylor@guardian.co.uk>
2016-08-25 12:17:20 +0100

    Add migrationKongId to Dynamo (set to None for now)

--- app/logic/DeveloperFormLogic.scala
+++ app/logic/DeveloperFormLogic.scala
@@ -27,7 +27,7 @@ class DeveloperFormLogic(dynamo: DB, kong: KongWrapper) {
       val newBonoboUser = BonoboUser(consumer.id, formData)
       dynamo.saveUser(newBonoboUser)
 
-      val newKongKey = KongKey(consumer.id, consumer, Tier.Developer.rateLimit, Tier.Developer, formData.productName, formData.productUrl)
+      val newKongKey = KongKey(consumer.id, consumer, None, Tier.Developer.rateLimit, Tier.Developer, formData.productName, formData.productUrl)
       dynamo.saveKey(newKongKey, List.empty)
     }
 
