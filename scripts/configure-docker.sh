3ec9076f30806266e247e58d5fad60f8c5cbe89a
Ara Hayrabedian <ara.hayrabedian@gmail.com>
2016-08-25 18:33:33 +0300

    restore original addressing system for initializing kong

--- scripts/configure-docker.sh
+++ scripts/configure-docker.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+docker_host=${DOCKER_HOST?Docker host is not set}
+
+container_host=$(echo ${docker_host} | sed -e 's/tcp:\/\/\(.*\):.*/\1/')
+
 echo Starting Services ...
 docker-compose up -d
 
 echo Adding API ...
-wget -O - http://127.0.0.1:8001/apis --post-data 'name=internal&request_host=foo.com&upstream_url=http://example.com' --retry-connrefused --no-verbose
+wget -O - http://${container_host}:8001/apis --post-data 'name=internal&request_host=foo.com&upstream_url=http://example.com' --retry-connrefused --no-verbose
 
 echo Activating key-auth plugin ...
-wget -O - http://127.0.0.1:8001/apis/internal/plugins/ --post-data 'name=key-auth' --retry-connrefused --no-verbose
+wget -O - http://${container_host}:8001/apis/internal/plugins/ --post-data 'name=key-auth' --retry-connrefused --no-verbose
