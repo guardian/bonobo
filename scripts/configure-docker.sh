b18f40d75055114e715ead00b3640ab86ff3ffc1
Ara Hayrabedian <ara.hayrabedian@gmail.com>
2016-08-25 17:26:22 +0300

    get docker-compose version working, still wrapped with a script but oh well

--- scripts/configure-docker.sh
+++ scripts/configure-docker.sh
@@ -1,44 +1,10 @@
 #! /bin/bash
 
-docker_host=${DOCKER_HOST?Docker host is not set}
-
-container_host=$(echo ${docker_host} | sed -e 's/tcp:\/\/\(.*\):.*/\1/')
-
-function start_service {
-    name=${1?Name parameter missing}
-    port_number=${2?Port parameter missing}
-
-    docker start ${name}
-
-    nc -z ${container_host} ${port_number}
-    while [ $? -ne 0 ]; do
-        echo Waiting for ${name} to start listening ...
-        sleep 1
-        nc -z ${container_host} ${port_number}
-    done
-}
-
-if docker ps | grep postgres -q; then
-    echo Postgres container already exists
-else
-    echo Creating Postgres container ...
-    docker create -p 5434:5432 -e "POSTGRES_USER=kong" -e "POSTGRES_DB=kong" --name postgres postgres:9.4
-fi
-
-if docker ps | grep kong -q; then
-    echo Kong container already exists
-else
-    echo Creating kong container ...
-    docker create -p 8000:8000 -p 8001:8001 -p 8443:8443 -p 7946:7946 -p 7946:7946/udp --name kong --link postgres:postgres -e "KONG_DATABASE=postgres" -e "KONG_PG_HOST=postgres" mashape/kong:0.9.0
-fi
-
-start_service postgres 5434
-start_service kong 8001
+echo Starting Services ...
+docker-compose up -d
 
 echo Adding API ...
-curl -sS -X POST http://${container_host}:8001/apis -d name=internal -d request_host=foo.com -d upstream_url=http://example.com
+wget -O - http://127.0.0.1:8001/apis --post-data 'name=internal&request_host=foo.com&upstream_url=http://example.com' --retry-connrefused --no-verbose
 
 echo Activating key-auth plugin ...
-curl -sS -X POST http://${container_host}:8001/apis/internal/plugins/ -d name=key-auth
-
-
+wget -O - http://127.0.0.1:8001/apis/internal/plugins/ --post-data 'name=key-auth' --retry-connrefused --no-verbose
