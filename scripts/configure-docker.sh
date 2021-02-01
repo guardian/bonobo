54d81a99590e0250f209ef00480150f2f9578f92
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2021-02-01 11:05:21 +0000

    Migrate local Kong from deprecated /apis to /services and /routes.
    Kong 0.13 deprecated the API concept splitting it into Services with Routes.
    https://github.com/Kong/kong/blob/master/CHANGELOG.md#0130---20180322
    
    Adjust our local setup to create a Service and Route.

--- scripts/configure-docker.sh
+++ scripts/configure-docker.sh
@@ -4,18 +4,30 @@ echo Starting Kong Docker images ...
 docker-compose up -d
 
 echo Waiting for Kong
-wget -O - http://localhost:8001/apis --retry-connrefused --no-verbose
+wget -O - http://localhost:8001/services --retry-connrefused --no-verbose
 echo Kong looks responsive
 
-echo Adding API ...
+echo Adding a Service ...
 curl -i -X POST \
-  --url http://localhost:8001/apis/ \
-  --data 'name=internal' \
-  --data 'hosts=foo.com' \
-  --data 'upstream_url=http://example.com'
+  --url http://localhost:8001/services/ \
+  --data 'name=content' \
+  --data 'url=http://capi.dev.guardianapis.com'
 
-echo Activating key-auth plugin ...
+# Defining a Route for this Service requires the service.id
+echo Querying for Service id
+SERVICE_ID=`curl -q http://localhost:8001/services | jq -r '.data[0].id'`
+
+echo Adding Route to Service
 curl -i -X POST \
-  --url http://localhost:8001/apis/internal/plugins/ \
+  --url http://localhost:8001/routes/ \
+  --data 'hosts[]=content.dev.guardianapis.com' \
+  --data "service.id=$SERVICE_ID"
+
+
+echo Activating key-auth plugin for route...
+# This has questionable idempotency
+ROUTE_ID=`curl -q http://localhost:8001/routes | jq -r '.data[0].id'`
+
+curl -i -X POST \
+  --url "http://localhost:8001/routes/$ROUTE_ID/plugins/" \
   --data 'name=key-auth'
-
