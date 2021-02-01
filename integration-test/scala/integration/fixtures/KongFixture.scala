116a0008aa32b8f3a1f1605792462ae010368fc1
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2021-02-01 18:37:50 +0000

    Update integration test Kong create API call to 0.14 format.

--- integration-test/scala/integration/fixtures/KongFixture.scala
+++ integration-test/scala/integration/fixtures/KongFixture.scala
@@ -35,7 +35,7 @@ trait KongFixture extends BeforeAndAfterAll { this: Suite =>
 
   private def configureKong(): Unit = {
     println("Registering the API with Kong")
-    s"curl -sS -X POST $kongUrl/apis -d name=$kongApiName -d request_host=foo.com -d upstream_url=http://example.com".!
+    s"curl -sS -X POST $kongUrl/apis -d name=$kongApiName -d hosts=foo.com -d upstream_url=http://example.com".!
 
     println("Enabling the key-auth plugin")
     s"curl -sS -X POST $kongUrl/apis/$kongApiName/plugins/ -d name=key-auth".!
