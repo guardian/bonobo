4db3558dcd5239dd021b158794b9647b437f594a
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2021-02-02 17:41:19 +0000

    Bohobo sets rate limit plugin configurations on Kong /service rather than deprecated /api.

--- app/kong/Kong.scala
+++ app/kong/Kong.scala
@@ -100,7 +100,7 @@ class KongClient(ws: WSClient, serverUrl: String, apiName: String) extends Kong 
   }
 
   private def setRateLimit(consumerId: String, rateLimit: RateLimits): Future[Unit] = {
-    ws.url(s"$serverUrl/apis/$apiName/plugins").post(Map(
+    ws.url(s"$serverUrl/services/$apiName/plugins").post(Map(
       "consumer_id" -> Seq(consumerId),
       "name" -> Seq(RateLimitingPluginName),
       "config.policy" -> Seq("local"),
@@ -131,7 +131,7 @@ class KongClient(ws: WSClient, serverUrl: String, apiName: String) extends Kong 
   }
 
   private def getRateLimitingPluginId(consumerId: String): Future[String] = {
-    ws.url(s"$serverUrl/apis/$apiName/plugins")
+    ws.url(s"$serverUrl/services/$apiName/plugins")
       .withQueryStringParameters(("consumer_id" -> s"$consumerId"), ("name" -> RateLimitingPluginName))
       .get().flatMap {
         response =>
@@ -154,7 +154,7 @@ class KongClient(ws: WSClient, serverUrl: String, apiName: String) extends Kong 
   def updateConsumerRateLimit(consumerId: String, newRateLimit: RateLimits): Future[Happy.type] = {
     getRateLimitingPluginId(consumerId) flatMap {
       pluginId =>
-        ws.url(s"$serverUrl/apis/$apiName/plugins/$pluginId").patch(Map(
+        ws.url(s"$serverUrl/plugins/$pluginId").patch(Map(
           "consumer_id" -> Seq(consumerId),
           "name" -> Seq(RateLimitingPluginName),
           "config.policy" -> Seq("local"),
