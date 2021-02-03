dfe8fa998b85bfcf86978b87642cbf1c9b06ec02
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2021-02-03 18:05:22 +0000

    Add migrated rate-limit plugins to new service.

--- scripts/migration-apis-to-services/code/migrate-plugins.py
+++ scripts/migration-apis-to-services/code/migrate-plugins.py
@@ -5,29 +5,30 @@ url = "http://localhost:8001/plugins?size=10"
 print(url)
 
 while url != "":
-        http = httplib2.Http()
-        content = http.request(url)[1]
-        print(content)
+    http = httplib2.Http()
+    content = http.request(url)[1]
 
-        parsed = json.loads(content)
+    parsed = json.loads(content)
 
-        data = parsed['data']
-        for plugin in data:
-                name = plugin['name']
-                if name == "rate-limiting":
-                        consumer_id = plugin['consumer_id']
-                        config = plugin['config']
-                        print(consumer_id)
+    data = parsed['data']
+    for plugin in data:
+        name = plugin['name']
+        if name == "rate-limiting":
+            consumer_id = plugin['consumer_id']
+            config = plugin['config']
+            print(consumer_id)
 
-                        request = {'name': name, 'consumer_id': consumer_id, 'config': config}
-                        print(json.dumps(request))
+            payload = {'name': name, 'consumer_id': consumer_id, 'config': config}
+            print(json.dumps(payload))
 
-                        # TODO Post this to /services/interal/plugins
+            resp, content = http.request("http://localhost:8001/services/internal/plugins",
+                    "PUT", body=json.dumps(payload),
+                    headers={'content-type':'application/json'})
+            print(request)
 
-        url = ""
-        if 'next' in parsed:
-                next = parsed['next']
-                print(next)
-                url = next
-
+    url = ""
+    if 'next' in parsed:
+        next = parsed['next']
+        print(next)
+        url = next
 
